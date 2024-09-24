c8d1126225d049a2f1b8012d47f17bf2bd3ab84b
SIDDHARTH <143601354+SID9927@users.noreply.github.com>
2024-09-24 14:52:32 +0530

    Update http-common-spring.js

--- src/http-common-spring.js
+++ src/http-common-spring.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:9090/FarmersMarketplace/',
+  baseURL: 'http://localhost:8080/FarmersMarketplace/',
   headers: {
     'Content-Type': 'application/json',
   },
