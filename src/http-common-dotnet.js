940d2fd0b7defc1b7a6f09ca0587409202faa52b
SIDDHARTH <143601354+SID9927@users.noreply.github.com>
2024-09-24 22:41:05 +0530

    Update http-common-dotnet.js

--- src/http-common-dotnet.js
+++ src/http-common-dotnet.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://localhost:7213',
+  // baseURL: 'https://localhost:7213',
+  baseURL: 'https://farmermarketplaceaspnetcore-production.up.railway.app/',
   headers: {
     'Content-Type': 'application/json',
   },
